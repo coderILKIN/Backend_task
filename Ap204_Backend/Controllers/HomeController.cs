405032953821ee2a219d2926ffea5c3fa3ce11e0
coderILKIN <tu758403c@code.edu.az>
2022-05-22 09:53:29 +0400

    add

---
+++ Ap204_Backend/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ap204_Backend.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index(int id)
+        {
+            //return Content(id.ToString());
+            return View();
+        }
+
+        public ActionResult About(int id)
+        {
+            //return Content(id.ToString());
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ(string name, string surname)
+        {
+            string fullname = $"{name} {surname}";
+
+            return Content(fullname);
+        }
+        
+
+        
+    }
+}
